f6e35312f9f7358ff6bfa087f692d5db5e92bbb9
STV <Steven.Hyles@hotmail.co.uk>
2016-03-03 20:08:56 +0000

    changed rakefile -slightly

--- rakefile.rb
+++ rakefile.rb
@@ -78,7 +78,7 @@ end
 #desc "Restoring nuget packages"
 task :restore do 
 	puts 'Restoring packages'
-  sh '.nuget\NuGet.exe restore .\Backend.sln -source "https://nuget.org/api/v2/"'
+        sh '.nuget\NuGet.exe restore .\Backend.sln -source "https://nuget.org/api/v2/"'
 	sh '.nuget\NuGet.exe restore .\Services.sln -source "https://nuget.org/api/v2/"'
 end
 
