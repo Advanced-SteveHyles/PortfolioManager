a5aa15d0c1586c084cdeb225596baee580f0ab36
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-03-08 12:35:46 +0000

    Added Panel for investments and started to wire up

--- PortfolioManager/ViewModels/InvestmentsTabsViewModel.cs
+++ PortfolioManager/ViewModels/InvestmentsTabsViewModel.cs
@@ -1,6 +1,42 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Forms.VisualStyles;
+using Portfolio.Common.DTO.DTOs;
+using PortfolioManager.Views.DataEntry;
+using PortfolioManager.Views.TabPanels;
+
 namespace PortfolioManager
 {
     public class InvestmentsTabsViewModel
     {
+        public object InvestmentList
+        {
+            get { return new InvestmentTabPanel() { DataContext = new InvestmentTabPanelViewModel() }; }
+        }
+
+
+    }
+
+    public class InvestmentTabPanelViewModel
+    {
+        public ObservableCollection<InvestmentDto> Investments
+        {
+            get
+            {
+                var investmentDtos = new List<InvestmentDto>()
+                {
+                    
+                    new InvestmentDto()
+                    {
+                        Class   ="Class"
+                    }                    
+                };
+                return new ObservableCollection<InvestmentDto>(investmentDtos) ;
+            }
+            set
+            {
+            }
+        }
+
     }
 }
