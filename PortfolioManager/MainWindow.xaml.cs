98626f03b5c4a970c591d92b051fa1f7d18f4604
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-03-08 08:28:41 +0000

    Added new Metaphor for switching between Portfolio and Investments

--- PortfolioManager/MainWindow.xaml.cs
+++ PortfolioManager/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using PortfolioManager.ViewModels.Menus;
+using PortfolioManager.Views;
+using PortfolioManager.Views.DataEntry;
+using PortfolioManager.Views.TabControls;
 
 namespace PortfolioManager
 {
@@ -12,9 +17,48 @@ namespace PortfolioManager
         {
             InitializeComponent();
 
-            this.PortfolioTabsView.DataContext = new PortfolioListTabViewModel();
-            this.InvestmentsTabsView.DataContext = new InvestmentsTabsViewModel();
+            this.DataContext = new MainWindowViewModel();
+           
             this.TopLevelMenu.DataContext = new TopLevelMenuViewModel();
+        }
+    }
+
+    public class MainWindowViewModel : ViewModel
+    {
+        private UserControl _mainContentArea;
+        private PortfolioTabsList _portfolioTabsList;
+        private InvestmentsTabsList _investmentTabsList;
+
+        public UserControl MainContentArea
+        {
+            get { return _mainContentArea; }
+            set
+            {
+                _mainContentArea = value;
+                OnPropertyChanged("MainContentArea");
+            }
+        }
+
+        public ICommand ShowPortfolioScreenCommand => new RelayCommand(ShowPortfolioScreen);
+
+        private void ShowPortfolioScreen()
+        {
+            if (_portfolioTabsList == null)
+            { 
+                _portfolioTabsList = new PortfolioTabsList() {DataContext = new PortfolioListTabViewModel()};
+            }
+            MainContentArea = _portfolioTabsList;
+        }
+
+        public ICommand ShowInvestmentScreenCommand => new RelayCommand(ShowInvestmentScreen);
+
+        private void ShowInvestmentScreen()
+        {
+            if (_investmentTabsList == null)
+            {
+                _investmentTabsList = new InvestmentsTabsList(){ DataContext = new InvestmentsTabsViewModel()};
+            }
+            MainContentArea = _investmentTabsList;
         }
     }
 }
