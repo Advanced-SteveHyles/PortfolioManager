b51b3a0888c09bfd60a0ae90e9d51e9d7233866a
STV <Steven.Hyles@hotmail.co.uk>
2016-03-12 09:33:04 +0000

    Added ConnectionString to local server

--- VirtualService/ApiConstants.cs
+++ VirtualService/ApiConstants.cs
@@ -4,11 +4,14 @@ namespace Portfolio.API.Virtual
     {        
         public const string WebapiPortfoliomanagercontext = "";
 
-          //public const string VirtualApiPortfoliomanagercontext =
-            //  @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=k:\PortfolioDatabase\PortfolioManagerDummy.mdf;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
+        //public const string VirtualApiPortfoliomanagercontext =
+        //  @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=k:\PortfolioDatabase\PortfolioManagerDummy.mdf;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
 
         public const string VirtualApiPortfoliomanagercontext =
-         @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=c:\temp\PortfolioManagerDummy.mdf;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
+            @"Data Source=.\PowerHouse;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
+
+        //       public const string VirtualApiPortfoliomanagercontext =
+        //         @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=c:\temp\PortfolioManagerDummy.mdf;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
 
     }
 }
