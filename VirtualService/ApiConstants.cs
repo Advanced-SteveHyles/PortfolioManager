e2bf95632b48ccf76894481dd3ddb87302393996
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-03-29 07:55:02 +0100

    Added project to handle database testing, added initial screen to capture portfolio valuation

--- VirtualService/ApiConstants.cs
+++ VirtualService/ApiConstants.cs
@@ -7,12 +7,11 @@ namespace Portfolio.API.Virtual
         //public const string VirtualApiPortfoliomanagercontext =
         //@"Data Source=.\PowerHouse;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
 
-             public const string VirtualApiPortfoliomanagercontext =
-        @"Data Source=.\PowerHouse;Initial Catalog=PortfolioLive;Integrated Security=True";
+             //public const string VirtualApiPortfoliomanagercontext =
+        //@"Data Source=.\PowerHouse;Initial Catalog=PortfolioLive;Integrated Security=True";
 
-
-        //public const string VirtualApiPortfoliomanagercontext =
-         //@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=c:\temp\PortfolioManagerDummy.mdf;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
+        public const string VirtualApiPortfoliomanagercontext =
+         @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=c:\temp\PortfolioManagerDummy.mdf;Initial Catalog=aspnet-Portfolio_API-20160118080906;Integrated Security=True";
 
     }
 }
