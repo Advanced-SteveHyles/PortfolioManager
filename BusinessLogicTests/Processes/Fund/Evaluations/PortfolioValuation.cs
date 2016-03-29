2733a37d44a139efd60cc23bde5b5a3f9e45c633
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-03-29 12:47:54 +0100

    Moved Processes to new folder.  Started Test for Portfoli valuations

---
+++ BusinessLogicTests/Processes/Fund/Evaluations/PortfolioValuation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicTests.FakeRepositories;
+using Xunit;
+
+namespace BusinessLogicTests.Processes.Fund.Evaluations
+{
+    public class PortfolioValuation
+    {
+        private FakeRepository _fakeRepository;
+
+        public PortfolioValuation()
+        {
+            _fakeRepository = new FakeRepository();
+        }
+
+        [Fact]
+        public void WhenAPortfolioHasNoAccountsTheValulationIsZero()
+        {
+            var portfolioValuation = portfolioValuation;
+
+
+            Assert(0, portfolioValuation.PropertyValue);
+        }
+    }
+}
